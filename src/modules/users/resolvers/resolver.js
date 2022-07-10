43e4dba9d47ccee0ecaba82a32e2b17e8b27e71e
foxnorth228 <khitrii03@gmail.com>
2022-07-10 15:38:45 +0300

    fix: fix problems with users

--- src/modules/users/resolvers/resolver.js
+++ src/modules/users/resolvers/resolver.js
@@ -1,60 +1,49 @@
 import "dotenv/config";
-import { sendRequest } from "../../../sendRequest.js"
+import { sendRequest, sendRequestWithParsing } from "../../../sendRequest.js"
 const url = process.env.USERS_URL;
 export let jwt = null;
 export const resolver = {
     Query: {
         jwt: () => jwt,
-        user:() => "name",
+        user: async (obj, args, context) => {
+            let answer = null;
+            try {
+                answer = await sendRequestWithParsing(`${url}${args.id}`);
+            } catch(err) {
+                console.log(err);
+            }
+            return answer;
+        },
     },
     Mutation: {
         register: async (obj, args) => { 
-            console.log(obj, args); 
+            let answer = null;
+            const body = {
+                firstName: args.user.firstName,
+                lastName: args.user.lastName,
+                password: args.user.password,
+                email: args.user.email,
+            }
             try {
-                const body = {
-                    firstName: args.firstName,
-                    lastName: args.lastName,
-                    password: args.password,
-                    email: args.email,
-                }
-                console.log(body);
-                console.log(`${url}register`);
-                await sendRequest(`${url}register`, "POST", body);
+                answer = await sendRequestWithParsing(`${url}register`, "POST", body);
             } catch(err) {
                 console.log(err.message);
             }
-            return args.name; 
+            return answer; 
         },
-        login: async (obj, args, context) => { 
-            console.log(obj, args); 
-            console.log("context", context.token)
+        login: async (obj, args) => { 
+            let answer = null;
+            const body = {
+                password: args.password,
+                email: args.email,
+            }
             try {
-                const body = {
-                    password: args.password,
-                    email: args.email,
-                }
-                console.log(body);
-                console.log(`${url}register`);
-                const answer = await sendRequest(`${url}login`, "POST",  body);
-                const jwtBody = JSON.parse(answer);
-                jwt = jwtBody["jwt"];
-                console.log(jwt);
+                answer = await sendRequestWithParsing(`${url}login`, "POST",  body);
+                jwt = answer["jwt"];
             } catch(err) {
                 console.log(err.message);
             }
-            return args.name; 
+            return answer["jwt"]; 
         },
-        verify: async () => {
-            try {
-                const body = {};
-                const headers = {
-                    'Authorization': `${jwt}`
-                };
-                await sendRequest(`${url}verify`, "POST", body, headers);
-            } catch(err) {
-                console.log(err.message);
-            }
-            return ""; 
-        }
     },
 };
