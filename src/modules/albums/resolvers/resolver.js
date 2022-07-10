d73c65b52ec6c6aacd483e2bad609690993b1b7e
foxnorth228 <khitrii03@gmail.com>
2022-07-10 17:11:07 +0300

    fix: fix problems with albums

--- src/modules/albums/resolvers/resolver.js
+++ src/modules/albums/resolvers/resolver.js
@@ -1,191 +1,128 @@
 import "dotenv/config";
-import { sendRequest } from "../../../sendRequest.js";
+import { deepUpdateProperties } from "../../../deepObjectCheckers.js";
+import { sendRequest, sendRequestWithParsing } from "../../../sendRequest.js";
 import { UserInputError, ForbiddenError } from "apollo-server";
-import { checkObjectForId, getObjectForId } from "../../../checkObjectExisting.js";
+import { checkObjectForId, getObjectForId, testObjectExisting } from "../../../checkObjectExisting.js";
 import { jwt } from "../../users/resolvers/resolver.js"
+import { checkAuth } from "../../../checkAuthorization.js";
 const url = process.env.ALBUMS_URL;
 export const resolver = {
     Query: {
         albums: async () => {
-            let albums = null;
+            let answer = null;
             try {
-                const answer = JSON.parse(await sendRequest(`${url}?limit=0`));
-                const albumsBody = JSON.parse(await sendRequest(`${url}?limit=${answer.total}`));
-                albums = albumsBody.items;
+                answer = await sendRequestWithParsing(`${url}?limit=0`);
+                answer = (await sendRequestWithParsing(`${url}?limit=${answer.total}`)).items;
             } catch(err) {
                 console.log(err);
             }
-            for (let album of albums) {
-                album.tracks = album.trackIds;
-                const newTracks = await getObjectForId(process.env.TRACKS_URL, 
-                    album.tracks, [[process.env.ARTISTS_URL, "artists"], [process.env.BANDS_URL, "bands"],
+            for (let el of answer) {
+                await deepUpdateProperties(el, "track", process.env.TRACKS_URL, [[process.env.ARTISTS_URL, "artists"], [process.env.BANDS_URL, "bands"],
                     [process.env.GENRES_URL, "genres"]]);
-                for (let i = 0; i < album.tracks.length; ++i) {
-                    album.tracks[i] = newTracks[i];
-                }
             }
-            for (let album of albums) {
-                album.artists = album.artistsIds;
-                const newArtists = await getObjectForId(process.env.ARTISTS_URL, 
-                    album.artists, [[process.env.BANDS_URL, "bands"], [process.env.GENRES_URL, "genres"]]);
-                for (let i = 0; i < album.artists.length; ++i) {
-                    album.artists[i] = newArtists[i];
-                }
+            for (let el of answer) {
+                await deepUpdateProperties(el, "artists", process.env.ARTISTS_URL, [[process.env.BANDS_URL, "bands"], [process.env.GENRES_URL, "genres"]]);
             }
-            for (let album of albums) {
-                album.bands = album.bandsIds;
-                const newBands = await getObjectForId(process.env.BANDS_URL, 
-                    album.bands, [[process.env.GENRES_URL, "genres"]]);
-                for (let i = 0; i < album.bands.length; ++i) {
-                    album.bands[i] = newBands[i];
-                }
+            for (let el of answer) {
+                await deepUpdateProperties(el, "bands", process.env.BANDS_URL, [[process.env.GENRES_URL, "genres"]]);
             }
-            for (let album of albums) {
-                album.genres = album.genresIds;
-                const newGenres = await getObjectForId(process.env.GENRES_URL, album.genres);
-                for (let i = 0; i < album.genres.length; ++i) {
-                    album.genres[i] = newGenres[i];
-                }
+            for (let el of answer) {
+                await deepUpdateProperties(el, "genres", process.env.GENRES_URL);
             }
-            return albums;
+            return answer;
         },
         album: async (obj, args) => {
-            let album = null;
+            let answer = null;
             try {
-                const answer = await sendRequest(`${url}${args.id}`, "GET");
-                album = JSON.parse(answer);
+                answer = await sendRequestWithParsing(`${url}${args.id}`, "GET");
             } catch(err) {
                 console.log(err);
             }
-            album.tracks = album.trackIds;
-            const newTracks = await getObjectForId(process.env.TRACKS_URL, 
-                album.tracks, [[process.env.ARTISTS_URL, "artists"], [process.env.BANDS_URL, "bands"],
-                [process.env.GENRES_URL, "genres"]]);
-            for (let i = 0; i < album.tracks.length; ++i) {
-                album.tracks[i] = newTracks[i];
-            }
-            album.artists = album.artistsIds;
-            const newArtists = await getObjectForId(process.env.ARTISTS_URL, 
-                album.artists, [[process.env.BANDS_URL, "bands"], [process.env.GENRES_URL, "genres"]]);
-            for (let i = 0; i < album.artists.length; ++i) {
-                album.artists[i] = newArtists[i];
-            }
-            album.bands = album.bandsIds;
-            const newBands = await getObjectForId(process.env.BANDS_URL, 
-                album.bands, [[process.env.GENRES_URL, "genres"]]);
-            for (let i = 0; i < album.bands.length; ++i) {
-                album.bands[i] = newBands[i];
-            }
-            album.genres = album.genresIds;
-            const newGenres = await getObjectForId(process.env.GENRES_URL, album.genres);
-            for (let i = 0; i < album.genres.length; ++i) {
-                album.genres[i] = newGenres[i];
-            }
-            return album;
+            console.log(answer);
+            await deepUpdateProperties(answer, "track", process.env.TRACKS_URL, [[process.env.ARTISTS_URL, "artists"], [process.env.BANDS_URL, "bands"],
+                    [process.env.GENRES_URL, "genres"]]);
+            await deepUpdateProperties(answer, "artists", process.env.ARTISTS_URL, [[process.env.BANDS_URL, "bands"], [process.env.GENRES_URL, "genres"]]);
+            await deepUpdateProperties(answer, "bands", process.env.BANDS_URL, [[process.env.GENRES_URL, "genres"]]);
+            await deepUpdateProperties(answer, "genres", process.env.GENRES_URL);
+            return answer;
         },
     },
     Mutation: {
-        createAlbum: async (obj, args) => {
+        createAlbum: async (obj, args, context) => {
+            await checkAuth(context.token);
             let answer = null;
             const body = {
                 name: args.album.name,
                 released: args.album.released,
                 artistsIds: args.album.artists,
                 bandsIds: args.album.bands,
-                trackIds: args.album.tracks,
+                trackIds: args.album.track,
                 genresIds: args.album.genres,
                 image: args.album.image,
             };
             const headers = {
-                "authorization": `${jwt}`,
+                "authorization": context.token,
             };
-            if(!(await checkObjectForId(process.env.TRACKS_URL, body.trackIds))) {
-                throw new UserInputError("Invalid argument value", {
-                    argumentsName: "id",
-                });
-            }
-            if(!(await checkObjectForId(process.env.BANDS_URL, body.bandsIds))) {
-                throw new UserInputError("Invalid argument value", {
-                    argumentsName: "id",
-                });
-            }
-            if(!(await checkObjectForId(process.env.ARTISTS_URL, body.artistsIds))) {
-                throw new UserInputError("Invalid argument value", {
-                    argumentsName: "id",
-                });
-            }
-            if(!(await checkObjectForId(process.env.GENRES_URL, body.genresIds))) {
-                throw new UserInputError("Invalid argument value", {
-                    argumentsName: "id",
-                });
-            }
+            await testObjectExisting(process.env.TRACKS_URL, body.trackIds);
+            await testObjectExisting(process.env.BANDS_URL, body.bandsIds);
+            await testObjectExisting(process.env.ARTISTS_URL, body.artistsIds);
+            await testObjectExisting(process.env.GENRES_URL, body.genresIds);
             try {
-                console.log(body);
-                answer = JSON.parse(await sendRequest(`${url}`, "POST", body, headers));
+                answer = await sendRequestWithParsing(`${url}`, "POST", body, headers);
+                await deepUpdateProperties(answer, "track", process.env.TRACKS_URL, [[process.env.ARTISTS_URL, "artists"], [process.env.BANDS_URL, "bands"],
+                    [process.env.GENRES_URL, "genres"]]);
+                await deepUpdateProperties(answer, "artists", process.env.ARTISTS_URL, [[process.env.BANDS_URL, "bands"], [process.env.GENRES_URL, "genres"]]);
+                await deepUpdateProperties(answer, "bands", process.env.BANDS_URL, [[process.env.GENRES_URL, "genres"]]);
+                await deepUpdateProperties(answer, "genres", process.env.GENRES_URL);
             } catch(err) {
                 console.log(err);
             }
-            if(answer.statusCode === 403) {
-                throw new ForbiddenError(answer.message);
-            }
-            return "";
+            return answer;
         },
-        deleteAlbum: (obj, args) => {
+        deleteAlbum: async (obj, args, context) => {
+            await checkAuth(context.token);
+            let answer = null;
             const body = {};
             const headers = {
-                "authorization": `${jwt}`,
+                "authorization": context.token,
             };
             try {
-                const answer = sendRequest(`${url}${args.id}`, "DELETE", body, headers);
+                answer = await sendRequestWithParsing(`${url}${args.id}`, "DELETE", body, headers);
             } catch(err) {
                 console.log(err);
             }
-            return "";
+            return answer;
         },
         updateAlbum: async (obj, args) => {
+            await checkAuth(context.token);
             let answer = null;
             const body = {
                 name: args.album.name,
                 released: args.album.released,
                 artistsIds: args.album.artists,
                 bandsIds: args.album.bands,
-                trackIds: args.album.tracks,
+                trackIds: args.album.track,
                 genresIds: args.album.genres,
                 image: args.album.image,
             };
             const headers = {
-                "authorization": `${jwt}`,
+                "authorization": context.token,
             };
-            if(!(await checkObjectForId(process.env.TRACKS_URL, body.trackIds))) {
-                throw new UserInputError("Invalid argument value", {
-                    argumentsName: "id",
-                });
-            }
-            if(!(await checkObjectForId(process.env.BANDS_URL, body.bandsIds))) {
-                throw new UserInputError("Invalid argument value", {
-                    argumentsName: "id",
-                });
-            }
-            if(!(await checkObjectForId(process.env.ARTISTS_URL, body.artistsIds))) {
-                throw new UserInputError("Invalid argument value", {
-                    argumentsName: "id",
-                });
-            }
-            if(!(await checkObjectForId(process.env.GENRES_URL, body.genresIds))) {
-                throw new UserInputError("Invalid argument value", {
-                    argumentsName: "id",
-                });
-            }
+            await testObjectExisting(process.env.TRACKS_URL, body.trackIds);
+            await testObjectExisting(process.env.BANDS_URL, body.bandsIds);
+            await testObjectExisting(process.env.ARTISTS_URL, body.artistsIds);
+            await testObjectExisting(process.env.GENRES_URL, body.genresIds);
             try {
-                console.log(body);
-                answer = JSON.parse(await sendRequest(`${url}${args.id}`, "PUT", body, headers));
+                answer = await sendRequestWithParsing(`${url}${args.id}`, "PUT", body, headers);
+                await deepUpdateProperties(answer, "track", process.env.TRACKS_URL, [[process.env.ARTISTS_URL, "artists"], [process.env.BANDS_URL, "bands"],
+                    [process.env.GENRES_URL, "genres"]]);
+                await deepUpdateProperties(answer, "artists", process.env.ARTISTS_URL, [[process.env.BANDS_URL, "bands"], [process.env.GENRES_URL, "genres"]]);
+                await deepUpdateProperties(answer, "bands", process.env.BANDS_URL, [[process.env.GENRES_URL, "genres"]]);
+                await deepUpdateProperties(answer, "genres", process.env.GENRES_URL);
             } catch(err) {
                 console.log(err);
             }
-            if(answer.statusCode === 403) {
-                throw new ForbiddenError(answer.message);
-            }
-            return "";
+            return answer;
         },
     },
 };
